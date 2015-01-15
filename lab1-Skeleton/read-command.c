d9d58da464cded3bfa422c62b9bb23b6e60815be
Nicholas Ma <nicholashma@gmail.com>
2015-01-15 02:05:51 -0800

    Some cleanup

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -11,7 +11,7 @@
 
 typedef struct node* node_t;
 
-// global variables
+// Global variables
 int cmd_read = 0;
 int cmd_total = 0;
 const char* if_cmd = "if ";
@@ -29,6 +29,16 @@ struct command_stream {
   node_t m_head;
   node_t m_curr;
   int stream_size;
+};
+
+enum token_type
+{
+  IF_TOKEN,
+  PIPE_TOKEN,
+  SEQUENCE_TOKEN,
+  SUBSHELL_TOKEN,
+  UNTIL_TOKEN,
+  WHILE_TOKEN,
 };
 
 // May need to be updated later
@@ -49,51 +59,21 @@ void insert_cmd(command_t cmd, command_stream_t cmd_stream)
   cmd_total++;
 }
 
-enum token_type
-{
-  IF_TOKEN,
-  PIPE_TOKEN,
-  SEQUENCE_TOKEN,
-  SUBSHELL_TOKEN,
-  UNTIL_TOKEN,
-  WHILE_TOKEN,
-};
-
-/* command_t make_cmd(char *word, token_type cmd_type)
+command_t make_cmd(char *word, token_type cmd_type)
 {
 
 }
 
-command_t simple_cmd(char **param)
+command_t make_simple_cmd(char *word)
 {
   command_t new_cmd = (command_t) checked_malloc(sizeof(struct command));
-  // set simple command; what parameters to use?
   new_cmd->type = SIMPLE_COMMAND;
   new_cmd->status = -1;
   new_cmd->input = NULL;
   new_cmd->output = NULL;
-  // set union stuff
-  // new_cmd->union word = param
+  new_cmd->u.word = word;
   return new_cmd;
 }
-
-command_t compound_cmd(command_t param)
-{
-  command_t new_cmd = (command_t) checked_malloc(sizeof(struct command));
-  // depending on if, until, while, sequence, pipe and set type appropriately
-  new_cmd->status = -1;
-  // set union stuff
-  return new_cmd;
-}
-
-command_t subshell_cmd();
-{
-  command_t new_cmd = (command_t) checked_malloc(sizeof(struct command));
-  new_cmd->type = SUBSHELL_COMMAND;
-  new_cmd->status = -1;
-  // set union stuff
-  return new_cmd;
-} */
 
 /* Read a command, add it to the command_stream. Check that it is a valid
    command first before adding the command in. */
@@ -123,7 +103,6 @@ bool is_token(char c)
       || (c == ')') || (c == '<') || (c == '>');
 }
 
-// compound command keyword checks
 bool if_check(const char *word)
 {
   for(int i = 0; i < 3; i++)
@@ -179,7 +158,7 @@ bool done_check(const char *word)
   return true;
 }
 
-// print error message
+// Print error message
 void print_err(int line)
 {
   fprintf(stderr, "%d: Incorrect syntax", line);
@@ -196,21 +175,21 @@ make_command_stream (int (*get_next_byte) (void *),
   char* check_stream[5];
   int buffer_size = 1024;
   int line = 1;
-  int char_stream_iter = 0;
+  int it = 0;
   bool newline_enc = false;
   enum token_type cmd_token;
 
-  // initialize
+  // Initialize
   command_stream_t cmd_stream = init_command_stream();
   char_stream = (char*) checked_malloc(buffer_size);
   buffer = (char*) checked_malloc(buffer_size);
-  // read in commmand
+  // Copy file to buffer
   for(int i = 0; (c = get_next_byte(get_next_byte_argument)) != EOF; i++)
   {
     if (i > buffer_size)
     {
       buffer_size += 1024;
-      buffer = (char*) realloc(buffer, buffer_size);
+      buffer = (char*) checked_realloc(buffer, buffer_size);
     }
     buffer[i] = c;
   }
@@ -218,9 +197,6 @@ make_command_stream (int (*get_next_byte) (void *),
     // GENERAL OUTLINE:
     // Iterate over stream and process each line as an expression
     // Check the whole command stream if each expression taken in is valid
-
-    // how do we take in lines as expressions?
-    // make a character array that takes in the whole line until newline/EOF
 
     // tokens: ; | ( ) < >
     // comments: # not preceded by token, followed by chars up to (not including) the newline
@@ -232,67 +208,72 @@ make_command_stream (int (*get_next_byte) (void *),
     // check syntax for command
       // if invalid syntax, report error and exit
     // add command to node
+
   for(int i = 0; i < strlen(buffer); i++)
   {
-    char_stream[char_stream_iter] = buffer[i];
-    // everytime we insert, reset after
+    char_stream[it] = buffer[i];
+    /* When we hit a newline, check for if, while, until keywords.
+       If we encounter a proper if, while, until, continue
+       storing bytes into char_stream until you hit corresponding
+       fi or done. Then, insert char_stream into a command node
+       and reset. */
     if (newline_enc)
     {
       switch(c)
       {
         case 'i':
-          for (; char_stream_iter < 3; char_stream_iter++)
-            char_stream[char_stream_iter] = buffer[i];
+          for (; it < 3; it++)
+            char_stream[it] = buffer[i];
           if (if_check(char_stream))
             for(; i < strlen(buffer) && buffer[i] != EOF; i++)
             {
-              char_stream[char_stream_iter] = buffer[i];
+              char_stream[it] = buffer[i];
               if (buffer[i] == '\n')
               {
                 if (fi_check(buffer, i + 1))
                 {
-                  char_stream[char_stream_iter + 1] = 'f';
-                  char_stream[char_stream_iter + 2] = 'i';
-                  //insert cmd into cmdstream and break
+                  char_stream[it + 1] = 'f';
+                  char_stream[it + 2] = 'i';
+                  // insert and reset and break
                 }
                 print_err(line);
               }
             }
         case 'w':
-          for (; char_stream_iter < 6; char_stream_iter++)
-            char_stream[char_stream_iter] = buffer[i];
+          for (; it < 6; it++)
+            char_stream[it] = buffer[i];
           if (while_check(char_stream))
             for(; i < strlen(buffer) && buffer[i] != EOF; i++)
             {
-              char_stream[char_stream_iter] = buffer[i];
+              char_stream[it] = buffer[i];
               if (buffer[i] == '\n')
               {
                 if (done_check(buffer, i + 1))
                 {
-                  char_stream[char_stream_iter + 1] = 'd';
-                  char_stream[char_stream_iter + 2] = 'o';
-                  char_stream[char_stream_iter + 3] = 'n';
-                  char_stream[char_stream_iter + 4] = 'e';
-                  //insert and reset and break
+                  char_stream[it + 1] = 'd';
+                  char_stream[it + 2] = 'o';
+                  char_stream[it + 3] = 'n';
+                  char_stream[it + 4] = 'e';
+                  // insert and reset and break
                 }
                 print_err(line);
               }
             }
         case 'u':
-          for (; char_stream_iter < 6; char_stream_iter++)
-            char_stream[char_stream_iter] = buffer[i];
+          for (; it < 6; it++)
+            char_stream[it] = buffer[i];
           if (while_check(char_stream))
             for(; i < strlen(buffer) && buffer[i] != EOF; i++)
             {
-              char_stream[char_stream_iter] = buffer[i];
+              char_stream[it] = buffer[i];
               if (buffer[i] == '\n')
               {
                 if (done_check(buffer, i + 1))
                 {
-                  char_stream[char_stream_iter + 1] = 'd';
-                  char_stream[char_stream_iter + 2] = 'o';
-                  char_stream[char_stream_iter + 3] = 'n';
-                  char_stream[char_stream_iter + 4] = 'e';
+                  char_stream[it + 1] = 'd';
+                  char_stream[it + 2] = 'o';
+                  char_stream[it + 3] = 'n';
+                  char_stream[it + 4] = 'e';
                   //insert and reset and break
                 }
                 print_err(line);
@@ -303,29 +284,15 @@ make_command_stream (int (*get_next_byte) (void *),
       }
       newline_enc = false;
     }
-    if (c == '\n')
+    // semi-colon behavior
+    // newline behavior
+    if (char_stream[it] == '\n')
     {
       line++;
       newline_enc = true;
       continue;
     }
-    if (c == '#' && !last_char_token)
-    {
-      do
-      {
-        c = get_next_byte(get_next_byte_argument);
-      } while(c != '\n' && c != EOF);
-      if (c == '\n')
-        line++;
-    }
-    if (is_token(c))
-    {
-      last_char_token = true;
-      if (c == '(')
-        left_parens++;
-      if (c == ')')
-        right_parens++;
-    }
+    // comment behavior
   }
   return cmd_stream;
 }
